16d7870fc7cf966cbee089ca40e5739af5de695c
rightgo09 <509139+rightgo09@users.noreply.github.com>
2023-03-07 00:47:03 +0900

    data.ts を大幅に簡略化

--- src/pages/api/data.ts
+++ src/pages/api/data.ts
@@ -1,26 +1,11 @@
-import type { NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { redis } from "lib/upstash";
 
-export const config = {
-  runtime: "edge",
-};
-
-type HatenaBookmark = {
-  title: string;
-  link: string;
-  description: string;
-  bookmarkcount: number;
-  imageUrl: string;
-  commentUrl: string;
-  category: string;
-};
-
-type HatenaBookmarks = {
-  [link: string]: HatenaBookmark;
-};
-
-export default async function handler(res: NextApiResponse<HatenaBookmarks>) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
   const ymdh = Math.floor(Date.now() / 3600000);
   const hbs = await redis.get(ymdh.toString());
-  res.status(200).json(JSON.parse(hbs as string) || "{}");
+  res.status(200).send(hbs);
 }
